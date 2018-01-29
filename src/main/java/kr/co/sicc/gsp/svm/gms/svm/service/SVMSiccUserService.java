b2cbdf7886f57c20c5138118e6a614fb2064a328
Hyunwoo.Kim <khw1231@sicc.co.kr>
2018-01-29 09:24:37 +0900

    경로 수정 com -> kr.co.sicc.gms.svm

--- src/main/java/kr/co/sicc/gsp/svm/gms/svm/service/SVMSiccUserService.java
+++ src/main/java/kr/co/sicc/gsp/svm/gms/svm/service/SVMSiccUserService.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
